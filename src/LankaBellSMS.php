c12a91fa1e8a1fb97142e96432b2b9d6c40b7fe5
LasanthaSudeera <lasan.sudeera@gmail.com>
2021-07-03 22:07:32 +0530

    Added Single text method

--- src/LankaBellSMS.php
+++ src/LankaBellSMS.php
@@ -7,9 +7,31 @@ use Illuminate\Support\Facades\Http;
 
 class LankaBellSMS
 {
+    public static function SendText(string $number, string $message)
+    {
+        if (\gettype($number) != 'string') throw new Exception('Phone number should be a string!');
+        if (\gettype($message) != 'string') throw new Exception('Message should be a string!');
+        if (\strlen($number) < 9)  throw new Exception('Minimum destination number length is 9.');
 
+        $LB_KEY = config('lankabell.LB_Key');
+        $LB_PORT = config('lankabell.LB_Port');
 
-    public static function TwoFactorAuth($number)
+        $response =  Http::withHeaders([
+            'Authorization' => $LB_KEY,
+        ])->get("http://smsm.lankabell.com:{$LB_PORT}/Sms.svc/SecureSendSms", [
+            'phoneNumber' => $number,
+            'smsMessage' => $message
+        ]);
+
+        $status =  $response->json()['Status'];
+        if ($status == "412" || $status == "410") throw new Exception('Invalid LankaBell LB Secure Key!');
+        if ($status == "601") throw new Exception('Minimum destination number length is 9.');
+
+        if ($status == "200") return true;
+        throw new Exception($response);
+    }
+
+    public static function TwoFactorAuth(string $number)
     {
         if (\gettype($number) != 'string') throw new Exception('Phone number should be a string!');
         if (\strlen($number) < 9)  throw new Exception('Minimum destination number length is 9.');
