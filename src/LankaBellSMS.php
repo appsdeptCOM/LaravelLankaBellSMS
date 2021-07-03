cd17425267e01ef88b8c02a820f261a901b4faf2
LasanthaSudeera <lasan.sudeera@gmail.com>
2021-07-03 19:59:15 +0530

    Added Basic 2Factor Auth method

--- src/LankaBellSMS.php
+++ src/LankaBellSMS.php
@@ -2,14 +2,29 @@
 
 namespace AppsDept\LaravelLankaBellSMS;
 
+use Illuminate\Support\Facades\Http;
+
 class LankaBellSMS
 {
 
-    public function __construct()
-    { }
 
-    public function getKey()
+    public static function TwoFactorAuth($number)
     {
-        return config('lankabell.LB_Key');
+        $LB_KEY = config('lankabell.LB_Key');
+        $TwoAuthCode = rand(100000, 999999);
+
+        $response =  Http::withHeaders([
+            'Authorization' => $LB_KEY,
+        ])->get('http://smsm.lankabell.com:4090/Sms.svc/SecureSendSms', [
+            'phoneNumber' => $number,
+            'smsMessage' => "Your verification code is {$TwoAuthCode}"
+        ]);
+
+        $status =  $response->json()['Status'];
+        if ($status == "412" || $status == "410") return "Invalid LB Secure Key";
+        if ($status == "601") return "Minimum destination number lenth is 9.";
+
+
+        if ($status == "200") return $TwoAuthCode;
     }
 }
